99e7179d3d0c32977c7f9cd19b2b7a278882a8b2
opopops <lemaildegermain@gmail.com>
2025-06-03 10:36:06 +0200

    feat(melange): pin version to 0.26.4

--- melange/src/melange/main.py
+++ melange/src/melange/main.py
@@ -24,7 +24,7 @@ class Melange:
         image: Annotated[str | None, Doc("wolfi-base image")] = (
             "cgr.dev/chainguard/wolfi-base:latest"
         ),
-        version: Annotated[str | None, Doc("Melange version")] = "latest",
+        version: Annotated[str | None, Doc("Melange version")] = "0.26.4",
         user: Annotated[str | None, Doc("Image user")] = "65532",
     ):
         """Constructor"""
