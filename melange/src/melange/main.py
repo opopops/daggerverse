b352411e8b8d0b5e2705e1f5e3db386fc35b5805
Germain GUEUTIER <germain.gueutier@gitguardian.com>
2025-06-25 22:51:58 +0200

    feat(melange): bump to 0.26.13

--- melange/src/melange/main.py
+++ melange/src/melange/main.py
@@ -24,7 +24,7 @@ class Melange:
         image: Annotated[str | None, Doc("wolfi-base image")] = (
             "cgr.dev/chainguard/wolfi-base:latest"
         ),
-        version: Annotated[str | None, Doc("Melange version")] = "0.26.12",
+        version: Annotated[str | None, Doc("Melange version")] = "0.26.13",
         user: Annotated[str | None, Doc("Image user")] = "65532",
     ):
         """Constructor"""
