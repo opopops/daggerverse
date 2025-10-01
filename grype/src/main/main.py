b55ede5b774666ad14ba21fe6d7921b2886aefa9
Germain GUEUTIER <lemaildegermain@gmail.com>
2025-10-01 08:41:30 +0200

    chore(grype): bump version to 0.100.0

--- grype/src/main/main.py
+++ grype/src/main/main.py
@@ -18,7 +18,7 @@ class Grype:
         image: Annotated[str | None, Doc("wolfi-base image")] = (
             "cgr.dev/chainguard/wolfi-base:latest"
         ),
-        version: Annotated[str | None, Doc("Grype version")] = "0.99.1",
+        version: Annotated[str | None, Doc("Grype version")] = "0.100.0",
         user: Annotated[str | None, Doc("Image user")] = "65532",
     ):
         """Constructor"""
